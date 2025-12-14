d64b2429456dd97eb78f29737b550905a4c47fbb
Oualid El Azzouzi <gthb112000@gmail.com>
2025-12-14 23:55:59 +0100

    Add ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-azzo <oel-azzo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 16:11:45 by oel-azzo          #+#    #+#             */
+/*   Updated: 2025/11/10 15:18:28 by oel-azzo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*s;
+	unsigned char		*d;
+
+	s = (const unsigned char *) src;
+	d = (unsigned char *) dest;
+	if (src == dest)
+		return (dest);
+	else if (dest > src)
+	{
+		d += n - 1;
+		s += n - 1;
+		while (n--)
+			*d-- = *s--;
+	}
+	else
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	return (dest);
+}
